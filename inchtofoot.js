1ed4afa330d2ee37ae88f06841bf9f700b207823
Sekh Sadi <freelancersekhsadi@gmail.com>
2023-06-23 07:03:15 -0700

    add to funcation

---
+++ inchtofoot.js
@@ -0,0 +1,17 @@
+// var inch = 156;
+
+// var feet = inch/12;
+// console.log(feet);
+
+function inchToFeet(inch){
+    var feet = inch/12;
+    return feet;
+}
+
+var naniFeet = inchToFeet(150);
+console.log(naniFeet);
+var nanaFeet = inchToFeet(156);
+console.log(nanaFeet);
+var dadiFeet = inchToFeet(250);
+console.log(dadiFeet);
+
