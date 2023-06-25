8e3f748e5e67af81424fec0dd4a7c97fa2dbf42a
Sekh Sadi <freelancersekhsadi@gmail.com>
2023-06-25 16:54:23 -0700

    add to factoical simple prartic file

--- constLet.js
+++ constLet.js
@@ -1,3 +1,12 @@
 // var name = 'sekh sadi';
 // console.log(name.length);
 
+var name = 'sekh sadi';
+if(name.length > 6){
+    name ='sari';
+}
+console.log(name);
+var age = 15;
+console.log(age);
+const country = 'Bangladesh';
+console.log(country);
---
+++ factoical.js
@@ -0,0 +1,6 @@
+var factoical = 1;
+
+for(i =1; i <= 5; i++){
+    var factoical = factoical * i;
+    console.log(i,factoical);
+}
